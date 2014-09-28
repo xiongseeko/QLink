67e9c335567a51386f745447ce19cd3f9c8b0e69
liushanshan <SANSAN@33deMacBook-Pro.local>
2014-09-28 13:00:40 +0800

    勾画照明类型 view

---
+++ QLink/MyView/Light1View.h
@@ -0,0 +1,13 @@
+//
+//  Light1View.h
+//  QLink
+//
+//  Created by 尤日华 on 14-9-28.
+//  Copyright (c) 2014年 SANSAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Light1View : UIView
+
+@end
