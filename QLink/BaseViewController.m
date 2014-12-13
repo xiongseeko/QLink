10500ff0410d7af12e6db33b55a2b4235f85300c
liushanshan <annlss@126.com>
2014-12-13 21:49:02 +0800

    改ip写死了

--- QLink/BaseViewController.m
+++ QLink/BaseViewController.m
@@ -112,7 +112,7 @@
         NSDictionary *dict = [NSDictionary dictionaryWithXMLData:newData];
         NSDictionary *info = [dict objectForKey:@"info"];
         zkConfig_ = [[Control alloc] init];
-        zkConfig_.Ip = @"110.83.121.191";//[info objectForKey:@"_ip"];
+        zkConfig_.Ip = [info objectForKey:@"_ip"];
         zkConfig_.SendType = [info objectForKey:@"_tu"];
         zkConfig_.Port = [info objectForKey:@"_port"];
         
